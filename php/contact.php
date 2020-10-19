0e0e8bc7f1527bf1db7387878fd15f4b7746cad0
omarsaleh11 <72376286+omarsaleh11@users.noreply.github.com>
2020-10-19 08:20:38 +0200

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,18 @@
 <?php
-    $name = $_POST['name'];
-    $mail = $_POST['mail'];
-    $subgect = $_POST['subgect'];
-    $massege = $_POST['massege'];
-    $to = "osaleh24724@gmail.com";
-    $headers = "Form: " . $name;
-    $txt = "you have received an e-mail from your website " . $name . ".\n\n" . $massege;
-    mail($mail, $subgect, $txt, $headers);
-    header("Location: contact.php?mailsend");
+if (isset($_POST['send'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['subgect'];
+	$massege = $_POST['massege'];
+
+	$to = 'osaleh24724@mail.com'; // Receiver Email ID, Replace with your email ID
+	$subject = 'Form Submission';
+	$message = "Name :" . $name . "\n" . "subgect :" . $subject . "\n" . "Wrote the following :" . "\n\n" . $massege;
+	$headers = "From: " . $email;
+
+	if (mail($to, $subject, $message, $headers)) {
+		echo "<h1>Sent Successfully! Thank you" . " " . $name . ", We will contact you shortly!</h1>";
+	} else {
+		echo "Something went wrong!";
+	}
+}
